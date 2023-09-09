eb0f006463b8b81ca034830d44be15f61e193e14
Varad Patil <varadapatil123@gmail.com>
2023-09-09 11:19:02 +0530

    logo change

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,7 +9,7 @@ import {
 import Avatar from "react-avatar";
 import { useBoardStore } from "@/store/BoardStore";
 import fetchSuggestion from "@/lib/fetchSuggestion";
-import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
+import { SignOutButton, SignedIn } from "@clerk/nextjs";
 
 interface Props {
   userImg: string | null;
@@ -39,30 +39,30 @@ function Header({ userImg, userName }: Props) {
     // fetchSuggestionFunc();
   }, [board]);
 
-  const trimmedUserName = userName!.length > 12
-  ? `${userName!.slice(0, 12)}...`
-  : userName;
+  const trimmedUserName =
+    userName!.length > 12 ? `${userName!.slice(0, 12)}...` : userName;
 
-  const  toggleDialog = ()=> {
+  const toggleDialog = () => {
     var dialog = document.getElementById("user-dialog");
     dialog?.classList.toggle("hidden");
-  }
+  };
 
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
         <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 z-[-50]" />
-        <Image
-          src="https://links.papareact.com/c2cdd5"
-          alt="Trello logo"
-          width={300}
-          height={100}
-          className="w-44 md:w-56 pb-5 md:pb-0 object-contain"
+        <div className="flex items-center pb-5 md:pb-0">
+        <img
+          src="https://cloud.appwrite.io/v1/storage/buckets/64f4194cd176c24c1d5a/files/64fc01b3ea5d9bd648fe/view?project=64f4165fc92e110bea14&mode=admin"
+          alt="logo"
+          className="w-16 h-16 md:w-20 md:h-20 rounded-full"
         />
+        <h1 className="font-bold text-4xl md:text-5xl pl-3 text-[#2A3956]">Organizify</h1>
+        </div>
         <div className="flex items-center space-x-4 justify-center md:justify-end w-full">
           <SignedIn>
             <SignOutButton>
-              <div className="flex cursor-pointer items-center text-light-1 hover:text-primary-500 mr-2">
+              <div className="flex cursor-pointer items-center text-light-1 hover:text-primary-500 hover:bg-slate-600 mr-2 p-2 rounded-full transform duration-300">
                 <ArrowLeftOnRectangleIcon
                   className="w-10 h-10"
                   name="Sign Out"
@@ -88,24 +88,23 @@ function Header({ userImg, userName }: Props) {
 
           {/* Avatar */}
           {userImg ? (
-            // <Image src={userImg} alt='User Image' width={50} height={50} className='rounded-full'/>
             <div className="relative inline-block">
-  <Image
-    src={userImg}
-    alt="User Image"
-    width={50}
-    height={50}
-    className="rounded-full cursor-pointer"
-    id="user-image"
-    onClick={toggleDialog}
-  />
-  <div
-    className="hidden absolute bottom-[-100%] w-[200px] left-[-100%] transform -translate-x-1/2 bg-slate-100 text-black p-2 rounded mt-2"
-    id="user-dialog"
-  >
-    Signed in as {trimmedUserName}
-  </div>
-</div>
+              <Image
+                src={userImg}
+                alt="User Image"
+                width={50}
+                height={50}
+                className="rounded-full cursor-pointer"
+                id="user-image"
+                onClick={toggleDialog}
+              />
+              <div
+                className="hidden absolute bottom-[-100%] w-[200px] left-[-100%] transform -translate-x-1/2 bg-slate-100 text-black p-2 rounded mt-2"
+                id="user-dialog"
+              >
+                Signed in as {trimmedUserName}
+              </div>
+            </div>
           ) : (
             <Avatar name="X" round size="50" color="#0055D1" />
           )}
